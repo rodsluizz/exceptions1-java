99d7ad077990605611ad4a645d2b4fe5f36536c3
rodsluizz <rodrigoluizcferreira@gmail.com>
2022-09-29 14:02:14 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,7 +12,7 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 	
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		System.out.print("Room number: ");
 		int number = sc.nextInt();
@@ -34,16 +34,12 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
+			String error = reservation.updateDates(checkIn, checkOut);
 			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			} 
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: check-out must be after check-in date");
-			} 
+			if(error != null) {
+				System.out.println("Error in reservation: " + error);
+			}
 			else {
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
 			}
 		}
